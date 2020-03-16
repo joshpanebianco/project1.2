5180079ce4d6862ae925cf6a0b3f5b17df2477bd
Josh <pana23@gmail.com>
2020-03-16 14:23:19 +1100

    heroku update

---
+++ app/models/community.rb
@@ -0,0 +1,4 @@
+class Community
+  belongs_to :account
+	validates_presence_of :url, :name, :bio
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :accounts
+
+  resources :communities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root to: "public#index"
+
 end
