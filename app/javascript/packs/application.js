59ca20120e92941e21e402a79c7006ac582e9a0e
Josh <pana23@gmail.com>
2020-03-17 15:30:13 +1100

    updates to the styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,9 +21,9 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-require('@rails/ujs').start()
-require('turbolinks').start()
-require('@rails/activestorage').start()
-require('./channels')
+// require('@rails/ujs').start()
+// require('turbolinks').start()
+// require('@rails/activestorage').start()
+// require('./channels')
 
 require('bootstrap')
